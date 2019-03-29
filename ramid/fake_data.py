885a9aef6f441c2215fd91ef720a3dc7d141de57
mxh_yh <mxh403@163.com>
2019-03-29 10:53:28 +0800

    :sparkles: add Orders like talk python

---
+++ ramid/fake_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: mxh @time:2019/3/29 10:43
+"""
+
+
+def get_orders():
+    return [
+        {'name': 'Cereal', 'price': 4.99},
+        {'name': 'Cheese', 'price': 2.15},
+        {'name': 'Milk', 'price': 6.99},
+        {'name': 'Oranges', 'price': 2.54},
+        {'name': 'Apples', 'price': 1.99},
+        {'name': 'Bread', 'price': 2.99},
+    ]
--- ramid/views.py
+++ ramid/views.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
 
+from ramid.fake_data import get_orders
+
 
 @view_config(route_name='home', renderer="templates/mytemplate.pt")
-def my_view(request):
-    return {'project': 'ramid'}
+def my_view(_):
+    return {'project': 'ramid', "orders": get_orders()}
